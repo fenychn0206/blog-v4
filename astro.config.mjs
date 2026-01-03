88f0bc11905e85fd40601ed2e12b43267bcb4cbe
fenychn0206 <hey@ohdragonboi.cn>
2026-01-03 18:31:00 +0800

    feat(posts): GitHub风格Markdown - 添加了alert语法，角标语法 - 修复了thinking-of-movie-dead-to-rights的遗留语法

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,15 @@ import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
 import { SITE_URL } from "./src/consts";
+import remarkGfm from "remark-gfm";
+import remarkGithubBlockquoteAlert from "remark-github-blockquote-alert";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
   integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
+    remarkPlugins: [remarkGfm, remarkGithubBlockquoteAlert],
     shikiConfig: {
       themes: {
         light: "catppuccin-latte",
